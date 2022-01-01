e3ce53b71bb6fa85834d1e83bbecc7d581c48d5b
kna27 <krisharora27@gmail.com>
2022-01-01 18:37:15 -0500

    Clean up colorKeyboard method

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ let game = gameInput.value;
 
 // csgo
 movement = ["+moveleft", "+moveright", "+jumpthrow", "+back", "+forward", "+jump", "+duck", "noclip"]
-buy = ["buyammo1", "buyammo2", "autobuy", "rebuy"]
-commmunication = ["+voicerecord", "messagemode2", "messagemode", "radio", "+radialradio"]
-weapons = ["slot1", "slot2", "slot3", "slot4", "slot5", "slot6", "slot7", "slot8", "slot9", "buymenu", "+lookatweapon", "drop", "show_loadout_toggle", "lastinv", "+reload"]
+buy = ["buyammo1", "buyammo2", "autobuy", "rebuy", "buy "]
+commmunication = ["+voicerecord", "messagemode2", "messagemode", "radio", "+radialradio", "playerradio ", "say ", "say_team "]
+weapons = ["slot1", "slot2", "slot3", "slot4", "slot5", "slot6", "slot7", "slot8", "slot9", "buymenu", "+lookatweapon", "drop", "show_loadout_toggle", "lastinv", "+reload", "use weapon_"]
 
 fileInput.onchange = () => {
     const reader = new FileReader()
@@ -38,24 +38,39 @@ function parseCfg(c) {
 
 function colorKeyboard(b) {
     for (var key in b) {
+        let kb = b[key];
+        let fillColor = "#ff0";
+        switch (game) {
+            case "csgo":
+                if (document.getElementById(key)) {
+                    if (movement.has(kb)) {
+                        fillColor = "#f00"
+                    }
+                    else if (buy.has(kb)) {
+                        fillColor = "#0f0"
+                    }
+                    else if (commmunication.has(kb)) {
+                        fillColor = "#00f"
+                    } else if (weapons.has(kb)) {
+                        fillColor = "#0ff"
+                    }
+                }
+                break;
+            default:
+                fillColor = "#ff0"
+        }
         if (document.getElementById(key)) {
-            if (movement.includes(b[key])) {
-                document.getElementById(key).childNodes[1].setAttribute("fill", "#f00")
-            }
-            else if (b[key].startsWith("buy ") || buy.includes(b[key])) {
-                document.getElementById(key).childNodes[1].setAttribute("fill", "#0f0")
-            }
-
-            else if (b[key].startsWith("playerradio ") || b[key].startsWith("say ") || b[key].startsWith("say_team ") || commmunication.includes(b[key])) {
-                document.getElementById(key).childNodes[1].setAttribute("fill", "#00f")
-            } else if (weapons.includes(b[key]) || b[key].startsWith("use weapon_")) {
-                document.getElementById(key).childNodes[1].setAttribute("fill", "#0ff")
-            }
-            else {
-                document.getElementById(key).childNodes[1].setAttribute("fill", "#ff0")
-            }
-
-            console.log(key, b[key]);
+            document.getElementById(key).childNodes[1].setAttribute("fill", fillColor);
         }
     }
 }
+
+Array.prototype.has = function (s) {
+    let hasSubStr = false;
+    this.forEach(e => {
+        if (s.startsWith(e)) {
+            hasSubStr = true;
+        }
+    });
+    return hasSubStr || this.some(e => e.includes(s));
+};
